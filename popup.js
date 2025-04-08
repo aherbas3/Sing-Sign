cdbdb1603e7d5f56d9732fd0dbc5b88142155d20
ChristianSWill <Christian.SWillingham@gmail.com>
2025-04-08 11:24:18 -0400

    added sing-sign boxes

--- popup.js
+++ popup.js
@@ -8,46 +8,79 @@ function updateUI(running) {
 }
 
 function start() {
-    const style = document.createElement("style");
-    style.textContent = `
-        @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');
-        .sing-sign-sidebar {
-            --dark-gray: #2D2D2D;
-            background-color: var(--dark-gray);
-            padding: 16px 20px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: 100%;
-            height: 100%;
-        }
+    function createStyles () {
+        const fontLink = document.createElement("link");
+        fontLink.href = "https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap";
+        fontLink.rel = "stylesheet";
+        document.head.appendChild(fontLink);
 
-        .sing-sign-sidebar-inside {
-            display: flex;
-            width: 100%;
-            height: 100%;
-            flex-direction: column;
-            gap: 20px;
-            padding: 20px;
-            border-radius: 10px;
-            align-items: center;
-            background-color: white;
-        }
+        const style = document.createElement("style");
+        style.textContent = `
+            .sing-sign-sidebar {
+                --dark-gray: #2D2D2D;
+                --main: #2D2863;
+                background-color: var(--dark-gray);
+                padding: 16px 20px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                width: 100%;
+                height: 100%;
+                font-family: "Nunito", sans-serif;
+            }
 
-        .sing-sign-sidebar-inside > h1 {
-            color: black;
-            font-family: "Nunito", sans-serif;
-            font-size: 32px;
-            text-align: center;
-        }
+            .sing-sign-sidebar-inside {
+                width: 100%;
+                height: 100%;
+                gap: 20px;
+                padding: 20px;
+                border-radius: 10px;
+                background-color: white;
+                overflow-y: auto;
+                overflow-x: hidden;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
 
-        .sing-sign-box {
-            width: 200px;
-            height: 200px;
-            background-color: gray;
-        }
-    `;
-    document.head.appendChild(style);
+            .sing-sign-sidebar-inside > h1 {
+                color: black;
+                font-size: 32px;
+                text-align: center;
+            }
+
+            .sing-sign-box {
+                width: 100%;
+                max-width: 300px;
+                aspect-ratio: 15 / 11;
+                background-color: var(--main);
+                flex-shrink: 0;
+                border-radius: 10px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                gap: 8px;
+                padding: 5px 20px;
+            }
+
+            .sing-sign-box > h2 {
+                color: white;
+                font-size: 32px;
+            }
+
+            .sing-sign-box > img {
+                width: 100%;
+                max-width: 240px;
+                aspect-ratio: 16 / 9;
+                flex-shrink: 0;
+                object-fit: cover;
+            }
+        `;
+        document.head.appendChild(style);
+    }
+
+    createStyles();
 
     // create side bar
     const asideElement = document.querySelector(".XOawmCGZcQx4cesyNfVO");
@@ -55,11 +88,10 @@ function start() {
     sideDiv.classList.add("sing-sign-sidebar");
     const innerDiv = document.createElement("div");
     innerDiv.classList.add("sing-sign-sidebar-inside");
-    
 
     //add header
     const innerHeader = document.createElement("h1");
-    innerHeader.textContent = "Your signs will appear here!";
+    innerHeader.innerHTML = "Your signs will appear here!";
     innerDiv.appendChild(innerHeader);
 
     //add sider bar
@@ -80,16 +112,46 @@ function start() {
 
     asideElement.prepend(sideDiv);
 
-    // for (let i = 0; i < 3; i++) {
-    //     const signBox = document.createElement("div");
-    //     const signText = document.createElement("h1");
-    //     signBox.classList.add("sing-sign-box");
-    //     sideDiv.append(signBox);
-    // }
+    const data = [
+        {title: "organism", img: chrome.runtime.getURL("assets/organism-img.png")},
+        {title: "biology", img: chrome.runtime.getURL("assets/biology-img.png")},
+        {title: "beauty", img: chrome.runtime.getURL("assets/beauty-img.png")},
+        {title: "season", img: chrome.runtime.getURL("assets/season-img.png")},
+    ]
+
+    data.forEach(entry => {
+        const signBox = document.createElement("div");
+
+        const signText = document.createElement("h2");
+        signText.textContent = entry.title;
+        signBox.appendChild(signText);
+
+        const signImage = document.createElement("img");
+        signImage.setAttribute("src", entry.img);
+        signBox.appendChild(signImage);
+
+        signBox.classList.add("sing-sign-box");
+        innerDiv.append(signBox);
+    }) 
 
     document.querySelector(".XOawmCGZcQx4cesyNfVO > aside").style.display =
         "none";
+}
+
+function stop() {
+    if (window._singResizeObserver) {
+        window._singResizeObserver.disconnect();
+        window._singResizeObserver = null;
     }
+
+    const sideDiv = document.querySelector(".sing-sign-sidebar");
+    if (sideDiv) {
+        document.querySelector(".XOawmCGZcQx4cesyNfVO").removeChild(sideDiv);
+    }
+
+    document.querySelector(".XOawmCGZcQx4cesyNfVO > aside").style.display =
+        "block";
+}
 
 chrome.storage.local.get(["running"]).then((result) => {
   updateUI(result.running);
@@ -112,20 +174,7 @@ toggleBtn.addEventListener("click", async () => {
   } else {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: () => {
-        if (window._singResizeObserver) {
-          window._singResizeObserver.disconnect();
-          window._singResizeObserver = null;
-        }
-
-        const sideDiv = document.querySelector(".sing-sign-sidebar");
-        if (sideDiv) {
-          document.querySelector(".XOawmCGZcQx4cesyNfVO").removeChild(sideDiv);
-        }
-
-        document.querySelector(".XOawmCGZcQx4cesyNfVO > aside").style.display =
-          "block";
-      },
+      func: stop,
     });
   }
 
